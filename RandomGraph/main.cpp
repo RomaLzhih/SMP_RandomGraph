eb0462abac86a501114342b257f9deb805262cd0
Ziyang Men <791191656@qq.com>
2020-07-12 17:06:09 +0800

    7.12 开始造随即图

--- RandomGraph/main.cpp
+++ RandomGraph/main.cpp
@@ -17,17 +17,17 @@ using namespace std;
 typedef int NODE;
 const int MAXN = 1e6;
 string _filename =
-    "D:/aMain/git/test/data/random_graph/general_random/group_1/"
-    "n2000_t50_p3_b50/"
+    "D:/git/git/test/data/random_graph/general_random/group_1/"
+    "n1500_t30_p3_b002_v07/"
     "animal";
 
-const int NodesNum = 2000;          // total nodes number
+const int NodesNum = 1500;          // total nodes number
 const int pSize = 3;               // total partition number
-const int tSize = 50;              // terminal number in each partition
+const int tSize = 30;              // terminal number in each partition
 double subGnodeRatio = 0.7;  // V_k/V
-double subGarcRatio = 0.1;   // arc/maximum
+double subGarcRatio = 0.02;   // arc/maximum
 const int GraphNum = 20;          // graph number generate
-const int TreeSpanNodesNum = 10;
+const int TreeSpanNodesNum = 5;
 bool random_span = 1;
 
 /* G container */
@@ -138,6 +138,7 @@ void bfs(int root, int k) {
 void AddArcToSubgraph(int k) {
     // Select Terminal
     while (subGTerminalPool[k].size() < tSize) {
+		Sleep(10);
         int u = ReturnSetRandom_WithoutDelete(subGNodesPool[k]);
         if (subGTerminalPool[k].find(u) != subGTerminalPool[k].end()) {
             continue;
@@ -155,8 +156,7 @@ void AddArcToSubgraph(int k) {
         int v = ReturnSetRandom_WithoutDelete(subGNodesPool[k]);
         if (u > v) swap(u, v);
 
-        if (u == v ||
-            subGArcPool[k].find(make_pair(u, v)) != subGArcPool[k].end()) {
+        if (u == v) {
             continue;
         } else if (subGTerminalPool[k].count(u) &&
                    subGTerminalPool[k].count(v)) {
@@ -164,7 +164,7 @@ void AddArcToSubgraph(int k) {
         } else {
             subGArcPool[k].insert(make_pair(u, v));
         }
-		cout << subGArcPool[k].size() << endl;
+		//cout << subGArcPool[k].size() << endl;
     }
     return;
 }
@@ -282,9 +282,11 @@ int main() {
 
         // Build Subgraph
         for (int k = 1; k <= pSize; k++) {
-			subGnodenum = (int)(NodesNum * get_random_num(0.3, 0.7));
-			subGarcnum = (int)(subGnodenum * (subGnodenum - 1) / 2 * subGarcRatio);
-			//cout << subGarcnum << endl;
+			//subGnodenum = (int)(NodesNum * get_random_num(0.3, 0.7));
+			subGnodenum = (int)(NodesNum * subGnodeRatio);
+			subGarcnum = (int)(1.0*subGnodenum * (subGnodenum - 1) / 2 * subGarcRatio);
+			//cout << subGarcnum << endl;\
+
             _NodesPool = NodesPool;
             NODE root = ReturnSetRandom(_NodesPool);
             bfs(root, k);
